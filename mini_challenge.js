a7df1f9d1a3b065aa8e0fbef6d619f2ef28be36a
jcab118 <32029661+jcab118@users.noreply.github.com>
2017-10-05 11:11:06 -0400

    js mini challenge

---
+++ mini_challenge.js
@@ -0,0 +1,29 @@
+var num1 = 7;
+var num2 = 11;
+console.log(num1 + num2);
+
+
+var foodNames = ["wings", "pizza", "salad", "wraps",["lasagna", "tacos", "spagetti"]];
+console.log(wings, pizza);
+
+
+if (num1 < 100){
+ alert ("hey that's less 100");
+}
+else {
+  alert ("hey thats greater than 100");
+}
+
+
+var Name = prompt("Please enter your name here");
+alert("Hey name");
+
+
+function chooseDoor(doorNumber){
+	if(doorNumber ==1){
+		console.log("You won a paid vacation")
+		}else if (doorNumber ==2){
+			console.log("You won free tuition at NY Coding and Design Academy")
+			}else if (doorNumber ==3){
+				console.log("You won a brand new car")
+			}
